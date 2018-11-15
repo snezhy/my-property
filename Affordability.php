b25c5c1bc4e8f4499921d798bc363637ea8c9b4f
Snezhana <snezhy@rocketmail.com>
2018-11-15 19:41:30 +0000

    Add a new line for consistency

--- Affordability.php
+++ Affordability.php
@@ -14,6 +14,7 @@ class Affordability {
 
 
     public function __construct($recurringIncome, $recurringExpenses) {
+
         $this->recurringIncome = $recurringIncome;
         $this->recurringExpenses = $recurringExpenses;
     }
